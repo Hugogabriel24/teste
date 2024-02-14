1b221f02b0211ae9a598ea06b6b4959fc698f908
Hugo Gabriel <hugogabriel24@live.com>
2024-02-14 19:18:23 -0300

    Created Project

---
+++ src/main/java/com/aula/aprender/AulaSpsApplication.java
@@ -0,0 +1,13 @@
+package com.aula.aprender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaSpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaSpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aula/aprender/AulaSpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aula.aprender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaSpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
